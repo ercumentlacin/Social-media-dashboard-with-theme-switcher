d109a26f0154a2cd21f79d674d93e6eadb755567
ercumenlacin <1140104023@nku.edu.tr>
2021-01-26 19:00:48 +0300

    updated

--- src/components/CardMd/CardContainerMd.jsx
+++ src/components/CardMd/CardContainerMd.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./card-container-md.css";
 import CardMd from "./CardMd";
+import cardData from "../../data/cardData";
 
 const CardContainerMd = () => {
   const socialMd = ["facebook", "twitter", "instagram", "linkedin"];
-  const getSocialMd = socialMd.map((i) => <CardMd />);
+  const getSocialMd = cardData.map((socialMedia) => (
+    <CardMd
+      svg={socialMedia.svg}
+      nickname={socialMedia.nickname}
+      followersCount={socialMedia.followersCount}
+      type={socialMedia.type}
+      today={socialMedia.today}
+      increase={socialMedia.increase}
+    />
+  ));
   return <div className="card-container-md">{getSocialMd}</div>;
 };
 
--- src/components/CardMd/CardMd.jsx
+++ src/components/CardMd/CardMd.jsx
@@ -2,25 +2,32 @@ import React, { useContext } from "react";
 import ThemeContext from "../../context/ThemeContext";
 import "./cardMd.css";
 
-const CardMd = () => {
+const CardMd = (props) => {
+  const { svg, nickname, followersCount, type, today, increase } = props;
   const { theme } = useContext(ThemeContext);
   const { bg, topBg, cardBg, text, textWhite, isDark } = theme;
+  let increaseColor;
+  increase ? (increaseColor = "mediumseagreen") : (increaseColor = "indianred");
+
   return (
     <div style={{ backgroundColor: cardBg, color: text }} className="card-md">
       <div className="adress">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-          <path
-            fill="#178FF5"
-            d="M18.896 0H1.104C.494 0 0 .494 0 1.104v17.793C0 19.506.494 20 1.104 20h9.58v-7.745H8.076V9.237h2.606V7.01c0-2.583 1.578-3.99 3.883-3.99 1.104 0 2.052.082 2.329.119v2.7h-1.598c-1.254 0-1.496.597-1.496 1.47v1.928h2.989l-.39 3.018h-2.6V20h5.098c.608 0 1.102-.494 1.102-1.104V1.104C20 .494 19.506 0 18.896 0z"
-          />
-        </svg>
-        <span>@nathanf</span>
+        <img width={20} src={svg} alt="social media icon" />
+
+        <span>{nickname}</span>
       </div>
       <p>
-        <strong style={{ color: textWhite }}>1987</strong>
+        <strong style={{ color: textWhite }}>{followersCount}</strong>
       </p>
-      <p>FOLLOWERS</p>
-      <span>12 Today</span>
+      <p className="type">{type.toUpperCase()}</p>
+      <span style={{ color: increaseColor, fontWeight: 700 }}>
+        {increase ? (
+          <i className="fas fa-caret-up"></i>
+        ) : (
+          <i className="fas fa-caret-down"></i>
+        )}{" "}
+        {today} Today
+      </span>
     </div>
   );
 };
