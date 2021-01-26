29dd11329d377d56dd68aff85ecfccb94568dac1
ercumenlacin <1140104023@nku.edu.tr>
2021-01-26 19:30:10 +0300

    change icons

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -1,36 +1,37 @@
+import facebookSvg from "../assets/images/icon-facebook.svg";
+import instagramSvg from "../assets/images/icon-instagram.svg";
+import twitterSvg from "../assets/images/icon-twitter.svg";
+import youtubeSvg from "../assets/images/icon-youtube.svg";
+
 const cardData = [
   {
     increase: true,
-    svg:
-      "https://www.flaticon.com/svg/vstatic/svg/174/174848.svg?token=exp=1611676385~hmac=4cf6f343aa57ac184b791746f37eadba",
-    nickname: "@nathanf",
+    svg: facebookSvg,
+    nickname: "@ercumentlacin",
     followersCount: 1987,
     type: "followers",
     today: 12,
   },
   {
     increase: true,
-    svg:
-      "https://www.flaticon.com/svg/vstatic/svg/174/174876.svg?token=exp=1611676424~hmac=160e3e5ef3fba2136979276106c3b94c",
-    nickname: "@nathanf",
+    svg: instagramSvg,
+    nickname: "@ercumentlacin",
     followersCount: 1044,
     type: "followers",
     today: 99,
   },
   {
     increase: true,
-    svg:
-      "https://www.flaticon.com/svg/vstatic/svg/174/174855.svg?token=exp=1611676463~hmac=5864e28c4ad8de083082f298a5a9d2ec",
-    nickname: "@realnathanf",
+    svg: twitterSvg,
+    nickname: "@ercumentlacin",
     followersCount: "11k",
     type: "followers",
     today: 1099,
   },
   {
     increase: false,
-    svg:
-      "https://www.flaticon.com/svg/vstatic/svg/1384/1384060.svg?token=exp=1611676483~hmac=1966c3aa73f6b152e79b6c441974d3bc",
-    nickname: "Nathan F.",
+    svg: youtubeSvg,
+    nickname: "ercumentlacin",
     followersCount: "8239",
     type: "subscribers",
     today: 144,
